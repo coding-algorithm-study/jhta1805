5c31bcee45ec0ff7a8e423a454979defe5cc7075
jslee <dlwhdtjd098@naver.com>
2021-08-17 22:31:21 +0900

    level1 - 숫자문자열과 영단어 다른 사람 풀이 & 파이써닉하게 수정.

--- week1/course_30_lessons_81301.py
+++ week1/course_30_lessons_81301.py
@@ -28,6 +28,41 @@ def solution(s):
     return int(answer)
 
 
+def solution2(s):
+    string_to_numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    answer = s
+    for idx, string in enumerate(string_to_numbers):
+        answer = answer.replace(string, str(idx))
+
+    return int(answer)
+
+
+def other_solution2(s):
+    string_to_number = dict(
+        zero=0,
+        one=1,
+        two=2,
+        three=3,
+        four=4,
+        five=5,
+        six=6,
+        seven=7,
+        eight=8,
+        nine=9,
+    )
+    answer = s
+    for k, v in string_to_number.items():
+        answer = answer.replace(k, str(v))
+
+    return int(answer)
+
+
 if __name__ == '__main__':
-    # assert solution("one4seveneight") == 1478
+    assert solution("one4seveneight") == 1478
     assert solution("threeonezero") == 310
+
+    assert solution2("one4seveneight") == 1478
+    assert solution2("threeonezero") == 310
+
+    assert other_solution2("one4seveneight") == 1478
+    assert other_solution2("threeonezero") == 310
